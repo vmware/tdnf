51dce51b3f3a213a8cfd2bf658b834d5b1ed75bb
Priyesh  Padmavilasom <ppadmavilasom@vmware.com>
2017-02-19 06:18:44 +0000

    add cli lib
    Change-Id: I52b14eafe9e7ca4e1f9171b1bd20222c7467ee4e

--- tools/cli/main.c
+++ tools/cli/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 VMware, Inc. All Rights Reserved.
+ * Copyright (C) 2015-2017 VMware, Inc. All Rights Reserved.
  *
  * Licensed under the GNU General Public License v2 (the "License");
  * you may not use this file except in compliance with the License. The terms
@@ -20,6 +20,8 @@
  */
 
 #include "includes.h"
+
+TDNF_CLI_CONTEXT _context = {0};
 
 int main(int argc, char* argv[])
 {
@@ -53,10 +55,27 @@ int main(int argc, char* argv[])
         {"upgrade-to",         TDNFCliUpgradeCommand},
         {"updateinfo",         TDNFCliUpdateInfoCommand},
     };
+
     int nCommandCount = sizeof(arCmdMap)/sizeof(TDNF_CLI_CMD_MAP);
     const char* pszCmd = NULL;
     PTDNF pTdnf = NULL;
     int nFound = 0;
+
+    _context.pFnCheckLocal = TDNFCliInvokeCheckLocal;
+    _context.pFnCheckUpdate = TDNFCliInvokeCheckUpdate;
+    _context.pFnClean = TDNFCliInvokeClean;
+    _context.pFnCount = TDNFCliInvokeCount;
+    _context.pFnInfo = TDNFCliInvokeInfo;
+    _context.pFnList = TDNFCliInvokeList;
+    _context.pFnProvides = TDNFCliInvokeProvides;
+    _context.pFnRepoList = TDNFCliInvokeRepoList;
+    //Alter and resolve will address commands like
+    //install, upgrade, erase, downgrade, distrosync
+    _context.pFnAlter = TDNFCliInvokeAlter;
+    _context.pFnResolve = TDNFCliInvokeResolve;
+    _context.pFnSearch = TDNFCliInvokeSearch;
+    _context.pFnUpdateInfo = TDNFCliInvokeUpdateInfo;
+    _context.pFnUpdateInfoSummary = TDNFCliInvokeUpdateInfoSummary;
 
     dwError = TDNFCliParseArgs(argc, argv, &pCmdArgs);
     BAIL_ON_CLI_ERROR(dwError);
@@ -91,13 +110,15 @@ int main(int argc, char* argv[])
                 dwError = TDNFOpenHandle(pCmdArgs, &pTdnf);
                 BAIL_ON_CLI_ERROR(dwError);
 
+                _context.hTdnf = pTdnf;
+
                 if(pCmdArgs->nVerbose)
                 {
                     dwError = TDNFCliVerboseShowEnv(pCmdArgs);
                     BAIL_ON_CLI_ERROR(dwError);
                 }
 
-                dwError = arCmdMap[nCommandCount].pFnCmd(pTdnf, pCmdArgs);
+                dwError = arCmdMap[nCommandCount].pFnCmd(&_context, pCmdArgs);
                 BAIL_ON_CLI_ERROR(dwError);
                 break;
             }
@@ -125,7 +146,7 @@ cleanup:
     return dwError;
 
 error:
-    PrintError(dwError);
+    TDNFCliPrintError(dwError);
     if (dwError == ERROR_TDNF_CLI_NOTHING_TO_DO)
     {
         // Nothing to do should not return an error code
@@ -135,463 +156,7 @@ error:
 }
 
 uint32_t
-TDNFCliCleanCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-    TDNF_CLEANTYPE nCleanType = CLEANTYPE_NONE;
-    PTDNF_CLEAN_INFO pTDNFCleanInfo = NULL;
-    char** ppszReposUsed = NULL;
-
-    dwError = TDNFCliParseCleanArgs(pCmdArgs, &nCleanType);
-    BAIL_ON_CLI_ERROR(dwError);
-  
-    dwError = TDNFClean(pTdnf, nCleanType, &pTDNFCleanInfo);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    //Print clean info
-    printf("Cleaning repos:");
-    ppszReposUsed = pTDNFCleanInfo->ppszReposUsed;
-    while(*ppszReposUsed)
-    {
-        printf(" %s", *ppszReposUsed);
-        ++ppszReposUsed;
-    }
-
-    printf("\n");
-
-    if(pTDNFCleanInfo->nCleanAll)
-    {
-        printf("Cleaning up everything\n");
-    }
-
-cleanup:
-    if(pTDNFCleanInfo)
-    {
-        TDNFFreeCleanInfo(pTDNFCleanInfo);
-    }
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliCountCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-    uint32_t unCount = 0;
-  
-    dwError = TDNFCountCommand(pTdnf, &unCount);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    printf("Package count = %d\n", unCount);
-
-cleanup:
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliListCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-    PTDNF_PKG_INFO pPkgInfo = NULL;
-    PTDNF_PKG_INFO pPkg = NULL;
-    uint32_t unCount = 0;
-    uint32_t dwIndex = 0;
-    PTDNF_LIST_ARGS pListArgs = NULL;
-
-    #define MAX_COL_LEN 256
-    char szNameAndArch[MAX_COL_LEN] = {0};
-    char szVersionAndRelease[MAX_COL_LEN] = {0};
-
-    #define COL_COUNT 3 
-    //Name.Arch | Version-Release | Repo
-    int nColPercents[COL_COUNT] = {55, 25, 15};
-    int nColWidths[COL_COUNT] = {0};
-
-    dwError = TDNFCliParseListArgs(pCmdArgs, &pListArgs);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    dwError = TDNFList(
-                  pTdnf,
-                  pListArgs->nScope,
-                  pListArgs->ppszPackageNameSpecs,
-                  &pPkgInfo,
-                  &unCount);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    dwError = GetColumnWidths(COL_COUNT, nColPercents, nColWidths);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    for(dwIndex = 0; dwIndex < unCount; ++dwIndex)
-    {
-        pPkg = &pPkgInfo[dwIndex];
-
-        memset(szNameAndArch, 0, MAX_COL_LEN);
-        if(snprintf(
-            szNameAndArch,
-            MAX_COL_LEN,
-            "%s.%s",
-            pPkg->pszName,
-            pPkg->pszArch) < 0)
-        {
-            dwError = errno;
-            BAIL_ON_CLI_ERROR(dwError);
-        }
-
-        memset(szVersionAndRelease, 0, MAX_COL_LEN);
-        if(snprintf(
-            szVersionAndRelease,
-            MAX_COL_LEN,
-            "%s-%s",
-            pPkg->pszVersion,
-            pPkg->pszRelease) < 0)
-        {
-            dwError = errno;
-            BAIL_ON_CLI_ERROR(dwError);
-        }
-
-        printf(
-            "%-*s%-*s%*s\n",
-            nColWidths[0],
-            szNameAndArch,
-            nColWidths[1],
-            szVersionAndRelease,
-            nColWidths[2],
-            pPkg->pszRepoName);
-    }
-
-cleanup:
-    if(pListArgs)
-    {
-        TDNFFreeListArgs(pListArgs);
-    }
-    if(pPkgInfo)
-    {
-        TDNFFreePackageInfoArray(pPkgInfo, unCount);
-    }
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliInfoCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-  
-    char* pszFormattedSize = NULL;
-
-    PTDNF_PKG_INFO pPkgInfo = NULL;
-    PTDNF_PKG_INFO pPkg = NULL;
-    PTDNF_LIST_ARGS pInfoArgs = NULL;
-
-    uint32_t unCount = 0;
-    uint32_t dwIndex = 0;
-    uint32_t unTotalSize = 0;
-
-    dwError = TDNFCliParseInfoArgs(pCmdArgs, &pInfoArgs);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    dwError = TDNFInfo(
-                  pTdnf,
-                  pInfoArgs->nScope,
-                  pInfoArgs->ppszPackageNameSpecs,
-                  &pPkgInfo,
-                  &unCount);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    for(dwIndex = 0; dwIndex < unCount; ++dwIndex)
-    {
-        pPkg = &pPkgInfo[dwIndex];
-
-        printf("Name          : %s\n", pPkg->pszName);
-        printf("Arch          : %s\n", pPkg->pszArch);
-        printf("Epoch         : %d\n", pPkg->dwEpoch);
-        printf("Version       : %s\n", pPkg->pszVersion);
-        printf("Release       : %s\n", pPkg->pszRelease);
-        printf("Install Size  : %s (%u)\n", pPkg->pszFormattedSize, pPkg->dwInstallSizeBytes);
-        printf("Repo          : %s\n", pPkg->pszRepoName);
-        printf("Summary       : %s\n", pPkg->pszSummary);
-        printf("URL           : %s\n", pPkg->pszURL);
-        printf("License       : %s\n", pPkg->pszLicense);
-        printf("Description   : %s\n", pPkg->pszDescription);
-
-        printf("\n");
-
-        unTotalSize += pPkg->dwInstallSizeBytes;
-    }
-  
-    dwError = TDNFUtilsFormatSize(unTotalSize, &pszFormattedSize);
-    BAIL_ON_CLI_ERROR(dwError);
-  
-    printf("\nTotal Size: %s (%u)\n", pszFormattedSize, unTotalSize);
-
-cleanup:
-    if(pInfoArgs)
-    {
-        TDNFFreeListArgs(pInfoArgs);
-    }
-    if(pPkgInfo)
-    {
-        TDNFFreePackageInfoArray(pPkgInfo, unCount);
-    }
-    TDNF_CLI_SAFE_FREE_MEMORY(pszFormattedSize);
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliRepoListCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-    PTDNF_REPO_DATA pRepos = NULL;
-    PTDNF_REPO_DATA pReposTemp = NULL;
-    TDNF_REPOLISTFILTER nFilter = REPOLISTFILTER_ENABLED;
-
-    dwError = TDNFCliParseRepoListArgs(pCmdArgs, &nFilter);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    dwError = TDNFRepoList(pTdnf, nFilter, &pRepos);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    pReposTemp = pRepos;
-    if(pReposTemp)
-    {
-        printf("%-20s%-40s%-10s\n", "repo id", "repo name", "status");
-    }
-    while(pReposTemp)
-    {
-        printf(
-            "%-20s%-40s%-10s\n",
-            pReposTemp->pszId,
-            pReposTemp->pszName,
-            pReposTemp->nEnabled ? "enabled" : "disabled");
-        pReposTemp = pReposTemp->pNext;
-    }
-
-cleanup:
-    TDNFFreeRepos(pRepos);
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliSearchCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t unError = 0;
-    PTDNF_PKG_INFO pPkgInfo = NULL;
-    PTDNF_PKG_INFO pPkg = NULL;
-    uint32_t unCount = 0;
-    uint32_t unIndex = 0;
-
-    unError = TDNFSearchCommand(pTdnf, pCmdArgs, &pPkgInfo, &unCount);
-    BAIL_ON_CLI_ERROR(unError);
-
-    for(unIndex = 0; unIndex < unCount; ++unIndex)
-    {
-        pPkg = &pPkgInfo[unIndex];
-        printf("%s : %s\n", pPkg->pszName, pPkg->pszSummary);
-    }
-
-cleanup:
-    TDNFFreePackageInfoArray(pPkgInfo, unCount);
-    return unError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliCheckLocalCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-
-    if(!pTdnf || !pCmdArgs)
-    {
-        dwError = ERROR_TDNF_CLI_INVALID_ARGUMENT;
-        BAIL_ON_CLI_ERROR(dwError);
-    }
-
-    if(pCmdArgs->nCmdCount < 2)
-    {
-        dwError = ERROR_TDNF_CLI_CHECKLOCAL_EXPECT_DIR;
-        BAIL_ON_CLI_ERROR(dwError);
-    }
-
-    dwError = TDNFCheckLocalPackages(pTdnf, pCmdArgs->ppszCmds[1]);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    fprintf(stdout, "Check completed without issues\n");
-
-cleanup:
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliProvidesCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-    PTDNF_PKG_INFO pPkgInfo = NULL;
-    PTDNF_PKG_INFO pPkgInfos = NULL;
-
-    if(!pTdnf || !pCmdArgs)
-    {
-        dwError = ERROR_TDNF_CLI_INVALID_ARGUMENT;
-        BAIL_ON_CLI_ERROR(dwError);
-    }
-
-    if(pCmdArgs->nCmdCount < 2)
-    {
-        dwError = ERROR_TDNF_CLI_PROVIDES_EXPECT_ARG;
-        BAIL_ON_CLI_ERROR(dwError);
-    }
-
-    dwError = TDNFProvides(pTdnf, pCmdArgs->ppszCmds[1], &pPkgInfos);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    pPkgInfo = pPkgInfos;
-    while(pPkgInfo)
-    {
-        fprintf(
-            stdout,
-            "%s-%s-%s.%s : %s\n",
-            pPkgInfo->pszName,
-            pPkgInfo->pszVersion,
-            pPkgInfo->pszRelease,
-            pPkgInfo->pszArch,
-            pPkgInfo->pszSummary);
-        fprintf(stdout, "Repo\t : %s\n", pPkgInfo->pszRepoName);
-        pPkgInfo = pPkgInfo->pNext;
-    }
-
-cleanup:
-    if(pPkgInfos)
-    {
-        TDNFFreePackageInfo(pPkgInfos);
-    }
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliCheckUpdateCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-    PTDNF_PKG_INFO pPkgInfo = NULL;
-    PTDNF_PKG_INFO pPkg = NULL;
-    uint32_t unCount = 0;
-    uint32_t dwIndex = 0;
-    char** ppszPackageArgs = NULL;
-    int nPackageCount = 0;
-
-    if(!pTdnf || !pCmdArgs)
-    {
-        dwError = ERROR_TDNF_CLI_INVALID_ARGUMENT;
-        BAIL_ON_CLI_ERROR(dwError);
-    }
-
-    dwError = TDNFCliParsePackageArgs(
-                  pCmdArgs,
-                  &ppszPackageArgs,
-                  &nPackageCount);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    dwError = TDNFCheckUpdates(
-                  pTdnf,
-                  ppszPackageArgs,
-                  &pPkgInfo,
-                  &unCount);
-    BAIL_ON_CLI_ERROR(dwError);
-
-    for(dwIndex = 0; dwIndex < unCount; ++dwIndex)
-    {
-        pPkg = &pPkgInfo[dwIndex];
-        printf("%*s\r", 80, pPkg->pszRepoName);
-        printf("%*s-%s\r", 50, pPkg->pszVersion, pPkg->pszRelease);
-        printf("%s.%s", pPkg->pszName, pPkg->pszArch);
-        printf("\n");
-    }
-
-cleanup:
-    TDNF_CLI_SAFE_FREE_STRINGARRAY(ppszPackageArgs);
-    if(pPkgInfo)
-    {
-        TDNFFreePackageInfoArray(pPkgInfo, unCount);
-    }
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-TDNFCliMakeCacheCommand(
-    PTDNF pTdnf,
-    PTDNF_CMD_ARGS pCmdArgs
-    )
-{
-    uint32_t dwError = 0;
-
-    if(!pTdnf || !pCmdArgs)
-    {
-        dwError = ERROR_TDNF_CLI_INVALID_ARGUMENT;
-        BAIL_ON_CLI_ERROR(dwError);
-    }
-    //Empty as refresh flag is set for makecache command
-    //and will execute refresh on all enabled repos
-
-    fprintf(stdout, "Metadata cache created.\n");
-
-cleanup:
-    return dwError;
-
-error:
-    goto cleanup;
-}
-
-uint32_t
-PrintError(
+TDNFCliPrintError(
     uint32_t dwErrorCode
     )
 {
@@ -633,38 +198,6 @@ error:
     goto cleanup;
 }
 
-uint32_t
-TDNFCliGetErrorString(
-    uint32_t dwErrorCode,
-    char** ppszError
-    )
-{
-    uint32_t dwError = 0;
-    char* pszError = NULL;
-    int i = 0;
-    int nCount = 0;
-    
-    TDNF_ERROR_DESC arErrorDesc[] = TDNF_CLI_ERROR_TABLE;
-
-    nCount = sizeof(arErrorDesc)/sizeof(arErrorDesc[0]);
-
-    for(i = 0; i < nCount; i++)
-    {
-        if (dwErrorCode == arErrorDesc[i].nCode)
-        {
-            dwError = TDNFAllocateString(arErrorDesc[i].pszDesc, &pszError);
-            BAIL_ON_CLI_ERROR(dwError);
-            break;
-        }
-    }
-    *ppszError = pszError;
-cleanup:
-    return dwError;
-error:
-    TDNF_CLI_SAFE_FREE_MEMORY(pszError);
-    goto cleanup;
-}
-
 void
 TDNFCliShowVersion(
     )
@@ -702,4 +235,157 @@ cleanup:
 
 error:
     goto cleanup;
+}
+
+uint32_t
+TDNFCliInvokeCheckLocal(
+    PTDNF_CLI_CONTEXT pContext,
+    const char *pszFolder
+    )
+{
+    return TDNFCheckLocalPackages(pContext->hTdnf, pszFolder);
+}
+
+uint32_t
+TDNFCliInvokeCheckUpdate(
+    PTDNF_CLI_CONTEXT pContext,
+    char** ppszPackageArgs,
+    PTDNF_PKG_INFO *ppPkgInfo,
+    uint32_t *pdwCount
+    )
+{
+    return TDNFCheckUpdates(pContext->hTdnf,
+                            ppszPackageArgs,
+                            ppPkgInfo,
+                            pdwCount);
+}
+
+uint32_t
+TDNFCliInvokeClean(
+    PTDNF_CLI_CONTEXT pContext,
+    TDNF_CLEANTYPE nCleanType,
+    PTDNF_CLEAN_INFO *ppTDNFCleanInfo
+    )
+{
+    return TDNFClean(pContext->hTdnf, nCleanType, ppTDNFCleanInfo);
+}
+
+uint32_t
+TDNFCliInvokeCount(
+    PTDNF_CLI_CONTEXT pContext,
+    uint32_t *pnCount
+    )
+{
+    return TDNFCountCommand(pContext->hTdnf, pnCount);
+}
+
+uint32_t
+TDNFCliInvokeAlter(
+    PTDNF_CLI_CONTEXT pContext,
+    TDNF_ALTERTYPE nAlterType,
+    PTDNF_SOLVED_PKG_INFO pSolvedPkgInfo
+    )
+{
+    return TDNFAlterCommand(pContext->hTdnf, nAlterType, pSolvedPkgInfo);
+}
+
+uint32_t
+TDNFCliInvokeInfo(
+    PTDNF_CLI_CONTEXT pContext,
+    PTDNF_LIST_ARGS pInfoArgs,
+    PTDNF_PKG_INFO *ppPkgInfo,
+    uint32_t *pdwCount
+    )
+{
+    return TDNFInfo(pContext->hTdnf,
+                   pInfoArgs->nScope,
+                   pInfoArgs->ppszPackageNameSpecs,
+                   ppPkgInfo,
+                   pdwCount);
+}
+
+uint32_t
+TDNFCliInvokeList(
+    PTDNF_CLI_CONTEXT pContext,
+    PTDNF_LIST_ARGS pListArgs,
+    PTDNF_PKG_INFO *ppPkgInfo,
+    uint32_t *pdwCount
+    )
+{
+    return TDNFList(pContext->hTdnf,
+                    pListArgs->nScope,
+                    pListArgs->ppszPackageNameSpecs,
+                    ppPkgInfo,
+                    pdwCount);
+}
+
+uint32_t
+TDNFCliInvokeProvides(
+    PTDNF_CLI_CONTEXT pContext,
+    const char *pszProvides,
+    PTDNF_PKG_INFO *ppPkgInfos
+    )
+{
+    return TDNFProvides(pContext->hTdnf, pszProvides, ppPkgInfos);
+}
+
+uint32_t
+TDNFCliInvokeRepoList(
+    PTDNF_CLI_CONTEXT pContext,
+    TDNF_REPOLISTFILTER nFilter,
+    PTDNF_REPO_DATA *ppRepos
+    )
+{
+    return TDNFRepoList(pContext->hTdnf, nFilter, ppRepos);
+}
+
+uint32_t
+TDNFCliInvokeResolve(
+    PTDNF_CLI_CONTEXT pContext,
+    TDNF_ALTERTYPE nAlterType,
+    PTDNF_SOLVED_PKG_INFO *ppSolvedPkgInfo
+    )
+{
+    return TDNFResolve(pContext->hTdnf, nAlterType, ppSolvedPkgInfo);
+}
+
+uint32_t
+TDNFCliInvokeSearch(
+    PTDNF_CLI_CONTEXT pContext,
+    PTDNF_CMD_ARGS pCmdArgs,
+    PTDNF_PKG_INFO *ppPkgInfo,
+    uint32_t *pdwCount
+    )
+{
+    return TDNFSearchCommand(pContext->hTdnf, pCmdArgs, ppPkgInfo, pdwCount);
+}
+
+uint32_t
+TDNFCliInvokeUpdateInfo(
+    PTDNF_CLI_CONTEXT pContext,
+    PTDNF_UPDATEINFO_ARGS pInfoArgs,
+    PTDNF_UPDATEINFO *ppUpdateInfo
+    )
+{
+    return TDNFUpdateInfo(
+               pContext->hTdnf,
+               pInfoArgs->nMode,
+               pInfoArgs->nScope,
+               pInfoArgs->ppszPackageNameSpecs,
+               ppUpdateInfo);
+}
+
+uint32_t
+TDNFCliInvokeUpdateInfoSummary(
+    PTDNF_CLI_CONTEXT pContext,
+    TDNF_AVAIL nAvail,
+    PTDNF_UPDATEINFO_ARGS pInfoArgs,
+    PTDNF_UPDATEINFO_SUMMARY *ppSummary
+    )
+{
+    return TDNFUpdateInfoSummary(
+               pContext->hTdnf,
+               nAvail,
+               pInfoArgs->ppszPackageNameSpecs,
+               ppSummary);
 }
