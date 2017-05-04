450521aa0e2d462353f890153a6b6fabcabac6b1
Priyesh  Padmavilasom <ppadmavilasom@vmware.com>
2017-05-04 01:55:53 +0000

    update to use new version of hawkey
    Change-Id: I03b98ff68d0db9612d897f3111beba32953939ef

--- client/api.c
+++ client/api.c
@@ -168,7 +168,7 @@ TDNFCheckLocalPackages(
     }
     fprintf(stdout, "Checking all packages from: %s\n", pszLocalPath);
 
-    hSack = hy_sack_create(NULL, NULL, NULL, 0);
+    hSack = hy_sack_create(NULL, NULL, NULL, NULL, 0);
     if(!hSack)
     {
         dwError = ERROR_TDNF_INVALID_PARAMETER;
