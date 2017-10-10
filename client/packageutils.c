303b82ea727841e5bd6f3013436e9091f8d2fc64
Priyesh Padmavilasom <ppadmavilasom@vmware.com>
2017-10-10 00:02:32 +0000

    add protected packages to obsoletes path
    Change-Id: I599bee849b1dce3a744d2bf0db4bcf270df3df62
    (cherry picked from commit 771948ff1cdeb17ef508ac314fec4bc3605b3f79)

--- client/packageutils.c
+++ client/packageutils.c
@@ -1108,3 +1108,57 @@ cleanup:
 error:
     goto cleanup;
 }
+
+uint32_t
+TDNFHasProtectedPkgsInList(
+    PTDNF_PKG_INFO pPkgInfo
+    )
+{
+    uint32_t dwError = 0;
+
+    if(!pPkgInfo)
+    {
+        dwError = ERROR_TDNF_INVALID_PARAMETER;
+        BAIL_ON_TDNF_ERROR(dwError);
+    }
+
+    for(; pPkgInfo; pPkgInfo = pPkgInfo->pNext)
+    {
+        if(pPkgInfo->pszName && !strcmp(pPkgInfo->pszName, TDNF_NAME))
+        {
+            dwError = ERROR_TDNF_SELF_ERASE;
+            BAIL_ON_TDNF_ERROR(dwError);
+        }
+    }
+error:
+    return dwError;
+}
+
+uint32_t
+TDNFCheckProtectedPkgs(
+    PTDNF_SOLVED_PKG_INFO pSolvedPkgInfo
+    )
+{
+    uint32_t dwError = 0;
+
+    if(!pSolvedPkgInfo)
+    {
+        dwError = ERROR_TDNF_INVALID_PARAMETER;
+        BAIL_ON_TDNF_ERROR(dwError);
+    }
+
+    if(pSolvedPkgInfo->pPkgsToRemove)
+    {
+        dwError = TDNFHasProtectedPkgsInList(pSolvedPkgInfo->pPkgsToRemove);
+        BAIL_ON_TDNF_ERROR(dwError);
+    }
+
+    if(pSolvedPkgInfo->pPkgsObsoleted)
+    {
+        dwError = TDNFHasProtectedPkgsInList(pSolvedPkgInfo->pPkgsObsoleted);
+        BAIL_ON_TDNF_ERROR(dwError);
+    }
+
+error:
+    return dwError;
+}
