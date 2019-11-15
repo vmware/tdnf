64dbb3b93181e0c71ac243313b9bb47c6eba351d
Siddharth Chandrasekaran <csiddharth@vmware.com>
2019-11-15 14:48:12 +0530

    pytest: add tdnf clean tests
    Signed-off-by: Siddharth Chandrasekaran <csiddharth@vmware.com>

---
+++ pytests/tests/test_clean.py
@@ -0,0 +1,41 @@
+#            (c) 2019 VMware Inc.,
+#
+#   Author: Siddharth Chandrasekaran <csiddharth@vmware.com>
+
+import pytest
+
+@pytest.fixture(scope='module', autouse=True)
+def setup_test_cache(utils):
+    yield
+    teardown_test_cache(utils)
+
+def teardown_test_cache(utils):
+    pass
+
+def test_clean_no_args(utils):
+    ret = utils.run([ 'tdnf', 'clean' ])
+    assert (ret['retval'] == 135)
+
+def test_clean_invalid_arg(utils):
+    ret = utils.run([ 'tdnf', 'clean', 'abcde' ])
+    assert (ret['retval'] == 133)
+
+def test_clean_packages(utils):
+    ret = utils.run([ 'tdnf', 'clean', 'packages' ])
+    assert (ret['retval'] == 248)
+
+def test_clean_dbcache(utils):
+    ret = utils.run([ 'tdnf', 'clean', 'dbcache' ])
+    assert (ret['retval'] == 248)
+
+def test_clean_metadata(utils):
+    ret = utils.run([ 'tdnf', 'clean', 'metadata' ])
+    assert (ret['retval'] == 248)
+
+def test_clean_expire_cache(utils):
+    ret = utils.run([ 'tdnf', 'clean', 'expire-cache' ])
+    assert (ret['retval'] == 248)
+
+def test_clean_plugins(utils):
+    ret = utils.run([ 'tdnf', 'clean', 'plugins' ])
+    assert (ret['retval'] == 248)
