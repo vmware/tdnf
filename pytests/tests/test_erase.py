849f929b7c6c3d16e4e5f2ab44747b75819ae6e8
Siddharth Chandrasekaran <csiddharth@vmware.com>
2019-12-03 18:41:06 +0530

    pytests: Add install and erase tests
    Signed-off-by: Siddharth Chandrasekaran <csiddharth@vmware.com>

---
+++ pytests/tests/test_erase.py
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2019 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the GNU General Public License v2 (the "License");
+# you may not use this file except in compliance with the License. The terms
+# of the License are located in the COPYING file of this distribution.
+#
+#   Author: Siddharth Chandrasekaran <csiddharth@vmware.com>
+
+import os
+import tempfile
+import pytest
+
+@pytest.fixture(scope='module', autouse=True)
+def setup_test(utils):
+    yield
+    teardown_test(utils)
+
+def teardown_test(utils):
+    pass
+
+def install_package(utils, pkgname, pkgversion):
+    if pkgversion:
+        pkg = pkgname + '-' + pkgversion
+    else:
+        pkg = pkgname
+    utils.run([ 'tdnf', 'install', '-y', '--nogpgcheck', pkg ])
+    assert(utils.check_package(pkgname) == True)
+
+def test_erase_no_arg(utils):
+    ret = utils.run([ 'tdnf', 'erase' ])
+    assert (ret['stderr'][0] == 'Nothing to do.')
+
+def test_erase_invalid_package(utils):
+    ret = utils.run([ 'tdnf', 'erase', 'invalid_package' ])
+    assert (ret['retval'] == 1011)
+
+def test_erase_package_with_version_suffix(utils):
+    pkgname = utils.config["mulversion_pkgname"]
+    pkgversion = utils.config["mulversion_lower"]
+    utils.install_package(pkgname, pkgversion)
+
+    utils.run([ 'tdnf', 'erase', '-y', pkgname + '-' + pkgversion ])
+    assert(utils.check_package(pkgname) == False)
+
+def test_erase_package_without_version_suffix(utils):
+    pkgname = utils.config["mulversion_pkgname"]
+    utils.install_package(pkgname)
+
+    utils.run([ 'tdnf', 'erase', '-y', pkgname ])
+    assert(utils.check_package(pkgname) == False)
