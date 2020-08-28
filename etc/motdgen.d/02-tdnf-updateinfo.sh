6fd37a46340fe34b3f4eccd7a712d4d2bc4486f4
Shreenidhi Shedi <sshedi@vmware.com>
2020-08-28 19:13:03 +0530

    Added tdnf-automatic
    Added correspoding tests for this feature
    
    Reorganized directories & files
    
    Signed-off-by: Shreenidhi Shedi <sshedi@vmware.com>

---
+++ etc/motdgen.d/02-tdnf-updateinfo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2020 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the GNU General Public License v2 (the "License");
+# you may not use this file except in compliance with the License. The terms
+# of the License are located in the COPYING file of this distribution.
+#
+
+path="/var/cache/tdnf/cached-updateinfo.txt"
+
+if [ -s "${path}" ]; then
+    grep -qE 'Security|Bugfix|Enhancement' "${path}" || exit 0
+    echo; cat "${path}"; echo "Run 'tdnf updateinfo info' to see the details."
+else
+    echo "tdnf update info not available yet!"
+fi
