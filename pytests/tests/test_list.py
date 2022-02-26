4b60b2b11708796bd51aa752053cec2c3d806c30
Oliver Kurth <okurth@vmware.com>
2022-02-26 01:23:12 +0000

    add test for list scope options

--- pytests/tests/test_list.py
+++ pytests/tests/test_list.py
@@ -9,7 +9,7 @@
 import pytest
 
 
-@pytest.fixture(scope='module', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def setup_test(utils):
     yield
     teardown_test(utils)
@@ -31,6 +31,10 @@ def helper_test_list_sub_cmd(utils, sub_cmd):
 
 
 def test_list_top(utils):
+    spkg = utils.config["sglversion_pkgname"]
+    ret = utils.run(['tdnf', 'install', '-y', spkg])
+    assert(ret['retval'] == 0)
+
     ret = utils.run(['tdnf', 'list'])
     assert(ret['retval'] == 0)
     ret = utils.run(['tdnf', 'list', utils.config["sglversion_pkgname"]])
@@ -52,7 +56,20 @@ def test_list_sub_cmd(utils):
     helper_test_list_sub_cmd(utils, 'recent')
 
 
-def test_list_upgrades(utils):
+def test_list_options(utils):
+    spkg = utils.config["sglversion_pkgname"]
+    ret = utils.run(['tdnf', 'install', '-y', spkg])
+    assert(ret['retval'] == 0)
+
+    helper_test_list_sub_cmd(utils, '--all')
+    helper_test_list_sub_cmd(utils, '--installed')
+    helper_test_list_sub_cmd(utils, '--available')
+    helper_test_list_sub_cmd(utils, '--obsoletes')
+    helper_test_list_sub_cmd(utils, '--extras')
+    helper_test_list_sub_cmd(utils, '--recent')
+
+
+def test_list_notinstalled(utils):
     mpkg = utils.config["mulversion_pkgname"]
     mpkg_version = utils.config["mulversion_lower"]
     spkg = utils.config["sglversion_pkgname"]
@@ -64,8 +81,10 @@ def test_list_upgrades(utils):
     ret = utils.run(['tdnf', 'list', 'upgrades', mpkg])
     assert(len(ret['stdout']) == 1)
 
-    ret = utils.run(['tdnf', 'list', 'upgrades', 'invalid_package'])
-    assert(ret['retval'] == 1011)
+    for cmd in ['updates', 'upgrades', 'downgrades', '--updates', '--upgrades', '--downgrades']:
+        ret = utils.run(['tdnf', 'list', cmd, 'invalid_package'])
+        assert(ret['retval'] == 1011)
 
-    ret = utils.run(['tdnf', 'list', 'upgrades', spkg])
-    assert(ret['retval'] == 1011)
+        # spkg is not installed, expect error
+        ret = utils.run(['tdnf', 'list', cmd, spkg])
+        assert(ret['retval'] == 1011)
