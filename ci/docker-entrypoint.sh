74f7bad5d5e9c065f716f83e6f093c35ee30228f
Oliver Kurth <okurth@gmail.com>
2022-06-16 17:00:16 -0700

    ci: exit if one of the cmake/make commands fail

--- ci/docker-entrypoint.sh
+++ ci/docker-entrypoint.sh
@@ -5,7 +5,7 @@ rm -rf build
 mkdir -p build
 cd build || exit 1
 
-cmake .. && make -j32 && make python -j32 && make check -j32
+cmake .. && make -j32 && make python -j32 && make check -j32 || exit
 
 if ! flake8 ../pytests ; then
   echo "flake8 tests failed"
