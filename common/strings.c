1d3f9ebf1cfae7e09c4fc30ed1076e7684ec4413
Oliver Kurth <okurth@gmail.com>
2023-03-17 13:36:52 -0700

    add a few validations in common/utils

--- common/strings.c
+++ common/strings.c
@@ -143,7 +143,7 @@ TDNFSplitStringToArray(
     size_t nCount = 0;
     size_t nIndex = 0;
 
-    if (!pszBuf || IsNullOrEmptyString(pszSep))
+    if (!pszBuf || IsNullOrEmptyString(pszSep) || !pppszTokens)
     {
         dwError = ERROR_TDNF_INVALID_PARAMETER;
         BAIL_ON_TDNF_ERROR(dwError);
