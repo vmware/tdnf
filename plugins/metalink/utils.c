b637e31b9b9a2cfc0579f96300eb7c3d403d1235
Oliver Kurth <okurth@gmail.com>
2023-03-31 12:41:34 -0700

    fix copyright years

--- plugins/metalink/utils.c
+++ plugins/metalink/utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 VMware, Inc. All Rights Reserved.
+ * Copyright (C) 2021-2023 VMware, Inc. All Rights Reserved.
  *
  * Licensed under the GNU Lesser General Public License v2.1 (the "License");
  * you may not use this file except in compliance with the License. The terms
