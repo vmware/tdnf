eaf7d3c9fe20a77b80d2817a686dd8cc2bab1f8c
Shreenidhi Shedi <sshedi@vmware.com>
2023-07-19 12:56:05 +0530

    Break after getting first valid tdnf command
    Signed-off-by: Shreenidhi Shedi <sshedi@vmware.com>

--- tools/cli/main.c
+++ tools/cli/main.c
@@ -113,6 +113,7 @@ int main(int argc, char **argv)
             if (strcmp(pszCmd, arCmdMap[i].pszCmdName) == 0)
             {
                 pCmd = &arCmdMap[i];
+                break;
             }
         }
 
