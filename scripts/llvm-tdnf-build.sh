2ee56a69255e883ab559cd91dda3ec1f675aedca
Shreenidhi Shedi <sshedi@vmware.com>
2023-08-07 12:17:40 +0530

    Add helper script to build tdnf using llvm/clang
    Signed-off-by: Shreenidhi Shedi <sshedi@vmware.com>

---
+++ scripts/llvm-tdnf-build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+pkgs=(llvm-devel clang-devel)
+pkgs+=(which gcc cmake make)
+
+tdnf install -y ${pkgs[@]}
+
+export CC="$(which clang)"
+export CFLAGS="-Qunused-arguments -Wno-deprecated -Werror"
+
+[ -d build ] && rm -rf build
+mkdir -p build
+cd build || exit 1
+
+history_loc="/usr/lib/sysimage/tdnf"
+p=$(nproc)
+
+mkdir -p $history_loc
+cmake -DHISTORY_DB_DIR=$history_loc .. && \
+  make -j$p && \
+  make python -j$p && \
+  make check -j$p || exit 1
