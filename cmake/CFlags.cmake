dcbbe15c7b19d0892492bbbce05ed25ef9d51607
Oliver Kurth <oliver.kurth@broadcom.com>
2025-10-27 20:24:53 +0000

    use -fPIC properly, enable opverride for -D_FORTIFY_SOURCE

--- cmake/CFlags.cmake
+++ cmake/CFlags.cmake
@@ -44,11 +44,9 @@ set(EXTRA_WARN_CFLAGS
     -Walloc-zero -Walloca -Wtrampolines
 )
 
-# Extra security / hardening flags
-set(EXTRA_SECURITY_CFLAGS
-    -D_FORTIFY_SOURCE=2
+# Extra security / hardening flags for executables
+set(EXTRA_SECURITY_CFLAGS_EXE
     -fstack-clash-protection
-    -fcf-protection=full
     -fPIE
     -pie
     -Wl,-z,relro
@@ -56,6 +54,31 @@ set(EXTRA_SECURITY_CFLAGS
     -Wl,-z,noexecstack
     -fno-plt
 )
+
+# Extra security / hardening flags for shared libraries
+set(EXTRA_SECURITY_CFLAGS_SO
+    -fstack-clash-protection
+    -fPIC
+    -Wl,-z,relro
+    -Wl,-z,now
+    -Wl,-z,noexecstack
+    -fno-plt
+)
+
+# Extra security / hardening flags for static libraries (no linker flags, no LTO)
+set(EXTRA_SECURITY_CFLAGS_STATIC
+    -fstack-clash-protection
+    -fPIC
+    -fno-plt
+    -fno-lto
+)
+
+# Add architecture-specific flags
+if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64|amd64")
+    list(APPEND EXTRA_SECURITY_CFLAGS_EXE -fcf-protection=full)
+    list(APPEND EXTRA_SECURITY_CFLAGS_SO -fcf-protection=full)
+    list(APPEND EXTRA_SECURITY_CFLAGS_STATIC -fcf-protection=full)
+endif()
 
 # Build-type dependent flags
 set(DEBUG_CFLAGS
@@ -72,22 +95,65 @@ set(FEATURE_FLAGS
     -D_DEFAULT_SOURCE
 )
 
-### Combine all flags
-set(TDNF_CFLAGS
+# Add _FORTIFY_SOURCE if not already defined by the build system
+# Check if _FORTIFY_SOURCE is already in the compiler flags
+string(FIND "${CMAKE_C_FLAGS}" "_FORTIFY_SOURCE" FORTIFY_POS)
+if(FORTIFY_POS EQUAL -1)
+    list(APPEND FEATURE_FLAGS -D_FORTIFY_SOURCE=2)
+endif()
+
+### Combine all flags for executables
+set(TDNF_CFLAGS_EXE
     ${WARN_CFLAGS}
     ${OPTIMIZE_CFLAGS}
     ${SECURITY_CFLAGS}
     ${EXTRA_WARN_CFLAGS}
-    ${EXTRA_SECURITY_CFLAGS}
+    ${EXTRA_SECURITY_CFLAGS_EXE}
+    ${FEATURE_FLAGS}
+)
+
+### Combine all flags for shared libraries
+set(TDNF_CFLAGS_SO
+    ${WARN_CFLAGS}
+    ${OPTIMIZE_CFLAGS}
+    ${SECURITY_CFLAGS}
+    ${EXTRA_WARN_CFLAGS}
+    ${EXTRA_SECURITY_CFLAGS_SO}
+    ${FEATURE_FLAGS}
+)
+
+### Combine all flags for static libraries
+set(TDNF_CFLAGS_STATIC
+    ${WARN_CFLAGS}
+    ${OPTIMIZE_CFLAGS}
+    ${SECURITY_CFLAGS}
+    ${EXTRA_WARN_CFLAGS}
+    ${EXTRA_SECURITY_CFLAGS_STATIC}
     ${FEATURE_FLAGS}
 )
 
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-    list(APPEND TDNF_CFLAGS ${DEBUG_CFLAGS})
+    list(APPEND TDNF_CFLAGS_EXE ${DEBUG_CFLAGS})
+    list(APPEND TDNF_CFLAGS_SO ${DEBUG_CFLAGS})
+    list(APPEND TDNF_CFLAGS_STATIC ${DEBUG_CFLAGS})
 elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
-    list(APPEND TDNF_CFLAGS ${RELEASE_CFLAGS})
+    list(APPEND TDNF_CFLAGS_EXE ${RELEASE_CFLAGS})
+    list(APPEND TDNF_CFLAGS_SO ${RELEASE_CFLAGS})
+    list(APPEND TDNF_CFLAGS_STATIC ${RELEASE_CFLAGS})
 endif()
 
-foreach(flag IN LISTS TDNF_CFLAGS)
+# Apply flags to executables by default
+foreach(flag IN LISTS TDNF_CFLAGS_EXE)
     add_c_compiler_flag(${flag})
 endforeach()
+
+# Function to apply appropriate flags based on target type
+function(apply_tdnf_flags target_name target_type)
+    if(target_type STREQUAL "SHARED")
+        target_compile_options(${target_name} PRIVATE ${TDNF_CFLAGS_SO})
+    elseif(target_type STREQUAL "STATIC")
+        target_compile_options(${target_name} PRIVATE ${TDNF_CFLAGS_STATIC})
+    else()
+        target_compile_options(${target_name} PRIVATE ${TDNF_CFLAGS_EXE})
+    endif()
+endfunction()
