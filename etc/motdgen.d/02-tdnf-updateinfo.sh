e3203aa146f95c1076caf8d47e0b5f32731df931
Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
2025-11-28 00:26:04 +0530

    Refresh repodata cache only when triggered by motdgen service
    Signed-off-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
    Signed-off-by: Oliver Kurth <oliver.kurth@broadcom.com>

--- etc/motdgen.d/02-tdnf-updateinfo.sh
+++ etc/motdgen.d/02-tdnf-updateinfo.sh
@@ -10,33 +10,17 @@
 #
 
 path="/var/cache/tdnf/cached-updateinfo.txt"
-ref_file="/tmp/updateinfo_timestamp.$$"
-tdnf_command="tdnf -q --refresh updateinfo"
 
-update_interval="1 hour ago"
-update_timeout="15s"
-
-if [[ "$PAM_TYPE" == "close_session" ]]; then
+if [[ "$PAM_TYPE" = "close_session" ]]; then
     exit 0
 fi
 
-touch -d "$update_interval" "$ref_file"
-if [ ! -s "${path}" ] || [ "$path" -ot "$ref_file" ]; then
-    # /var/cache/tdnf may not exist, but we pipe the output to a file there
-    mkdir -p "$(dirname ${path})"
-    timeout "$update_timeout" $tdnf_command | grep -vE '^Refreshing|^Disabling' > "${path}"
-    tdnf_status=${PIPESTATUS[0]}
-    if [[ ${tdnf_status} == 124 ]]; then
-        echo "tdnf updateinfo timed out"
-        # remove any partial data
-        rm -f "${path}"
-        exit 0
-    fi
-    if [[ ${tdnf_status} != 0 ]]; then
-        echo "tdnf updateinfo failed with ${tdnf_status}"
-    fi
+# This will be set by motdgen.service
+TRIGGERED_BY_MOTD_TIMER="${TRIGGERED_BY_MOTD_TIMER:-0}"
+
+if [ "$TRIGGERED_BY_MOTD_TIMER" = "1" ]; then
+    tdnf -q --refresh updateinfo | grep -vE '^Refreshing|^Disabling' > "${path}"
 fi
-rm -f "$ref_file"
 
 if [ -s "${path}" ]; then
     grep -qE 'Security|Bugfix|Enhancement' "${path}" || exit 0
